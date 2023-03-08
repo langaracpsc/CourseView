ba6af418cdd88a12ebdc1b3e7ee0c1332b9a0a66
rishit-singh <rsrishitsingh@gmail.com>
2023-03-07 23:55:01 -0800

    Fixed caching bug.

--- Shared/CourseStack.cs
+++ Shared/CourseStack.cs
@@ -7,46 +7,9 @@ using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 namespace CourseView.Shared;
 
-public partial class CourseStack : ComponentBase
+public partial class CourseStack
 {
-    [Parameter] 
-    public Course[] Courses { get; set; }
-
-    public string Content;
-    
-    public async Task UpdateAsync()
-    { 
-        this.Courses = await CourseManager.FetchCourses();
-    }
-
-    public bool Initialized;
-   
-    public async ValueTask<ItemsProviderResult<Course>> GetCoures(ItemsProviderRequest request)
-    {
-        if (!this.Initialized)
-            await this.OnInitializedAsync();
-        
-        return new ItemsProviderResult<Course>(this.Courses, this.Courses.Length);
-    }
-    
-    
-    protected override async Task OnInitializedAsync()
-    {
-        if (!Initialized)
-        {
-            Console.WriteLine("Enter");
-            this.Courses = new Course[0];
-
-            this.Content = await (new HttpClient().GetStringAsync("http://localhost:5157/courses"));
-            this.Courses =
-                JsonSerializer.Deserialize<Course[]>(JsonSerializer.Deserialize<Hashtable>(this.Content)["Payload"]
-                    .ToString());
-
-            this.Initialized = true;
-            Console.WriteLine("Exit");
-        }
-    }
-
+  
     public CourseStack()
     {
     }
